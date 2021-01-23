097ca6ecd3b5833ef9ee62f59a8ba376447af90d
nachoelchido <nacho_hernandez@comunidad.unam.mx>
2021-01-23 11:04:26 -0600

    agregando readme

---
+++ app/src/main/java/com/example/firstapp/models/Participante.kt
@@ -0,0 +1,10 @@
+package com.example.firstapp.models
+
+data class Participante(
+    val id: Int = 0,
+    val name: String = "",
+    val lastname: String = "",
+    val age: Int = 18,
+    val career: String = "",
+    val biography: String = ""
+)
